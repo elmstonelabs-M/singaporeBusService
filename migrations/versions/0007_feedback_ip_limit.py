b152e3225f2736879c32a8d95ece3aca31ce4887
elmstonelabs-M <elmstonelabs@gmail.com>
2026-05-31 17:01:44 +0800

    Shorten feedback migration revision id

--- migrations/versions/0007_feedback_ip_limit.py
+++ migrations/versions/0007_feedback_ip_limit.py
@@ -1,6 +1,6 @@
 """add feedback client ip and daily limit support
 
-Revision ID: 0007_add_feedback_client_ip_limit
+Revision ID: 0007_feedback_ip_limit
 Revises: 0006_add_static_data_state
 Create Date: 2026-05-31 00:00:00
 """
@@ -8,7 +8,7 @@ Create Date: 2026-05-31 00:00:00
 import sqlalchemy as sa
 from alembic import op
 
-revision = "0007_add_feedback_client_ip_limit"
+revision = "0007_feedback_ip_limit"
 down_revision = "0006_add_static_data_state"
 branch_labels = None
 depends_on = None
